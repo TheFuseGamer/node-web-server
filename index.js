7e1616ea1577305ff685aa6250f5d96ac0a23e8a
Amir Halloul <amirhalloul@gmail.com>
2021-10-25 21:21:02 +0100

    Add cars and cars/{id} endpoints

--- index.js
+++ index.js
@@ -2,14 +2,60 @@ const http = require('http');
 
 const PORT = 3000;
 
+const cars = [
+    {
+        id: 0,
+        make: 'Ford',
+        model: 'Fiesta',
+        year: '2018'
+    },
+    {
+        id: 1,
+        make: 'Ford',
+        model: 'Focus',
+        year: '2019'
+    },
+    {
+        id: 2,
+        make: 'Ford',
+        model: 'Mustang',
+        year: '2021'
+    },
+    {
+        id: 3,
+        make: 'BMW',
+        model: 'X5',
+        year: '2022'
+    },
+    {
+        id: 4,
+        make: 'Audi',
+        model: 'A4',
+        year: '2022'
+    }
+];
+
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.write(JSON.stringify({
-        message: 'Hello world!',
-        status: 'ok',
-        createdAt: new Date().toISOString()
-    }));
-    res.end();
+    const urlParts = req.url.split('/');
+    if (urlParts[1] === 'cars') {
+        if (urlParts.length === 2) {
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(cars));
+        } else if (urlParts.length === 3) {
+            const carId = parseInt(urlParts[2]);
+            const car = cars.find(car => car.id === carId);
+            if (car) {
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify(car));
+            } else {
+                res.writeHead(404, { 'Content-Type': 'text/plain' });
+                res.end('Car not found');
+            }
+        }
+    } else  { 
+        res.statusCode = 404;
+        res.end('Not found');
+    }
 });
 
 server.listen(PORT, () => {
