66bef9438935469e99a0c71be66d4bba0fe8656c
Amir Halloul <amirhalloul@gmail.com>
2021-10-25 21:04:13 +0100

    Send JSON data instead of HTML

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const http = require('http');
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<h1>Hello World</h1>');
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.write(JSON.stringify({
+        message: 'Hello world!',
+        status: 'ok',
+        createdAt: new Date().toISOString()
+    }));
     res.end();
 });
 
